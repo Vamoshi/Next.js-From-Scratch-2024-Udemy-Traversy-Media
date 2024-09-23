1a7e927dee12984449ea95a6c207633703528c73
Vamoshi <64162877+Vamoshi@users.noreply.github.com>
2024-09-23 12:14:32 -0700

    feat(DefaultImage): create defaultimage component that extends nextimage

---
+++ src/components/DefaultImage.tsx
@@ -0,0 +1,21 @@
+import Image, { ImageProps } from 'next/image';
+import React from 'react';
+
+type CustomImageProps = Omit<ImageProps, 'alt'> & {
+    alt?: string
+}
+
+const DefaultImage: React.FC<CustomImageProps> = ({
+    alt = "Default Alt Text",
+    width = 100,
+    height = 100,
+    loading = 'lazy',
+    ...props
+}) => {
+    console.log('====================================');
+    console.log(alt);
+    console.log('====================================');
+    return <Image alt={alt} width={width} height={height} loading={loading} {...props} />;
+};
+
+export default DefaultImage;
