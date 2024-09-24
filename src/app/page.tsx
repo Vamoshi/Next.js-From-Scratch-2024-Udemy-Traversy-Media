55536be38ed7dd422c1778286d4d4bcb7d4b5f77
Vamoshi <64162877+Vamoshi@users.noreply.github.com>
2024-09-24 00:14:23 -0700

    feat(HomeProperties): add homeproperties section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero, InfoBoxes } from '@/components'
+import HomeProperties from '@/components/HomeProperties'
 import React from 'react'
 
 const HomePage = () => {
     return <>
         <Hero />
         <InfoBoxes />
+        <HomeProperties />
     </>
 }
 
