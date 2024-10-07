9862bf264754e35ae39df12859a7e8fd46f9b59f
Vamoshi <64162877+Vamoshi@users.noreply.github.com>
2024-10-07 00:06:32 -0700

    Update FeaturedProperties.tsx

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -9,7 +9,7 @@ const FeaturedProperties = async () => {
     const properties = await Property.find({ is_featured: true }).lean() as PropertyDocument[]
 
     return properties.length > 0 && (
-        <section className="bg-blue-50 mx-4 my-6 pb-10">
+        <section className="bg-blue-50 mx-4 my-6 px-4 py-6 pb-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
                     Featured Properties
